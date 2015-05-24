3a99269928221f78279d93359588fa7086476259
dextero <hukutizuviki+git@gmail.com>
2015-05-24 13:47:49 +0200

    Add free camera movement

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
 #include <fenv.h>
 #include <iostream>
+#include <string>
 
+#include <sandbox/utils/types.h>
+#include <sandbox/utils/timer.h>
 #include <sandbox/window/window.h>
+#include <sandbox/rendering/model.h>
 
 #include "kd_tree.h"
 
@@ -70,24 +74,66 @@ public:
     bool running() const { return mRunning; }
 };
 
-
 class TreeVisualizer
 {
 public:
     TreeVisualizer():
-        wnd(1440, 900)
-    {}
-
-    void show(const kd_tree<double> &)
+        wnd(1440, 900),
+        fpsDeltaTime(0.0f, 0.0f),
+        colorShader(gResourceMgr.getShader("proj_basic.vert", "color.frag")),
+        box("box.obj", colorShader)
     {
+        wnd.lockCursor();
+        wnd.hideCursor();
+
+        wnd.getCamera().lookAt(sb::Vec3(5.f, 5.f, 20.f), sb::Vec3(5.f, 5.f, 0.f));
+    }
+
+    void show(const kd_tree<double> &tree)
+    {
+        sb::Timer clock;
+
         while (wnd.isOpened()) {
+            float delta = clock.getSecondsElapsed();
+            clock.reset();
+
             handleInput();
-            draw();
+            update(delta);
+            draw(tree);
         }
     }
 
 private:
+    static constexpr float SPEED = 0.5f;
+    static constexpr float PHYSICS_UPDATE_STEP = 0.03f;
+    static constexpr float fpsUpdateStep = 1.f;   // update FPS-string every fpsUpdateStep seconds
+
     sb::Window wnd;
+    sb::Vec3 speed;
+
+    float fpsCounter;      // how many frames have been rendered in fpsUpdateStep time?
+    float fpsCurrValue;    // current FPS value
+    Accumulator fpsDeltaTime;
+    std::string fpsString;
+
+    std::shared_ptr<sb::Shader> colorShader;
+    sb::Model box;
+
+    void handleMouseMoved(const sb::Event& e)
+    {
+        if (wnd.hasFocus())
+        {
+            sb::Vec2i halfSize = wnd.getSize() / 2;
+            int pixelsDtX = (int)e.data.mouse.x - halfSize.x;
+            int pixelsDtY = (int)e.data.mouse.y - halfSize.y;
+
+            static const float ROTATION_SPEED = 1.0f;
+            sb::Radians dtX = sb::Radians(ROTATION_SPEED * (float)pixelsDtX / halfSize.x);
+            sb::Radians dtY = sb::Radians(ROTATION_SPEED * (float)pixelsDtY / halfSize.y);
+
+            wnd.getCamera().mouseLook(dtX, dtY);
+        }
+    }
 
     void handleInput()
     {
@@ -96,10 +142,50 @@ private:
         {
             switch (e.type)
             {
+            case sb::Event::MouseMoved:
+                handleMouseMoved(e);
+                break;
+            case sb::Event::WindowFocus:
+                if (e.data.focus)
+                {
+                    wnd.hideCursor();
+                    wnd.lockCursor();
+                }
+                else
+                {
+                    wnd.hideCursor(false);
+                    wnd.lockCursor(false);
+                    speed = sb::Vec3(0.0f, 0.0f, 0.0f);
+                }
+                break;
             case sb::Event::KeyPressed:
                 switch (e.data.key) {
+                case sb::Key::A: speed.x = -SPEED; break;
+                case sb::Key::D: speed.x = SPEED; break;
+                case sb::Key::S: speed.z = -SPEED; break;
+                case sb::Key::W: speed.z = SPEED; break;
+                case sb::Key::Q: speed.y = SPEED; break;
+                case sb::Key::Z: speed.y = -SPEED; break;
                 case sb::Key::Esc:
                     wnd.close();
+                    break;
+                default:
+                    break;
+                }
+                break;
+        case sb::Event::KeyReleased:
+                switch (e.data.key) {
+                case sb::Key::A:
+                case sb::Key::D:
+                    speed.x = 0.f;
+                    break;
+                case sb::Key::S:
+                case sb::Key::W:
+                    speed.z = 0.f;
+                    break;
+                case sb::Key::Q:
+                case sb::Key::Z:
+                    speed.y = 0.f;
                     break;
                 default:
                     break;
@@ -114,9 +200,37 @@ private:
         }
     }
 
-    void draw()
+    void update(float deltaSeconds)
+    {
+        fpsDeltaTime.update(deltaSeconds);
+        ++fpsCounter;
+
+        // FPS update
+        if (fpsDeltaTime.getValue() >= fpsUpdateStep)
+        {
+            fpsCurrValue = fpsCounter / fpsUpdateStep;
+            fpsString = "FPS = " + sb::utils::toString(fpsCurrValue);
+            fpsDeltaTime.update(-fpsUpdateStep);
+            fpsCounter = 0.f;
+        }
+
+        wnd.getCamera().moveRelative(speed);
+    }
+
+    void draw(const kd_tree<double> &tree)
     {
         wnd.clear(sb::Color(0.f, 0.f, 0.5f));
+
+        wnd.draw(box);
+
+        wnd.drawString(fpsString, { 0.0f, 0.0f },
+                       (fpsCurrValue > 30.f
+                           ? sb::Color::Green
+                           : (fpsCurrValue > 20.f ? sb::Color::Yellow
+                                                  : sb::Color::Red)),
+                       0);
+
+        wnd.display();
     }
 };
 
