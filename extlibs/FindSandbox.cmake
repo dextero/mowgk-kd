ce42c41336b98880da1dd063a75646fd5c681423
dextero <hukutizuviki+git@gmail.com>
2015-05-28 11:32:49 +0200

    Add X11 library to FindSandbox.cmake

--- extlibs/FindSandbox.cmake
+++ extlibs/FindSandbox.cmake
@@ -9,16 +9,19 @@ if(EXISTS ${CMAKE_CURRENT_LIST_DIR}/sandbox/lib/libsandbox.a)
     find_package(OpenGL REQUIRED)
     find_package(DevIL REQUIRED)
     find_package(GLEW REQUIRED)
+    find_package(X11 REQUIRED)
 
     set(SANDBOX_FOUND TRUE)
     set(SANDBOX_INCLUDE_DIR ${CMAKE_CURRENT_LIST_DIR}/sandbox/include
                             ${ASSIMP_INCLUDE_DIR}
                             ${OPENGL_INCLUDE_DIR}
                             ${IL_INCLUDE_DIR}
-                            ${GLEW_INCLUDE_DIR})
+                            ${GLEW_INCLUDE_DIR}
+                            ${X11_INCLUDE_DIR})
     set(SANDBOX_LIBRARIES ${CMAKE_CURRENT_LIST_DIR}/sandbox/lib/libsandbox.a
                           ${ASSIMP_LIBRARIES}
                           ${OPENGL_LIBRARIES}
                           ${IL_LIBRARIES} ${ILU_LIBRARIES}
-                          ${GLEW_LIBRARY})
+                          ${GLEW_LIBRARY}
+                          ${X11_LIBRARIES})
 endif()
