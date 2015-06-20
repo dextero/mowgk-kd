9c9cba8e11dd8c33b86daaf7956d2c68f7d76b2d
Jakub Sawicki <kuba@jswk.pl>
2015-06-20 22:17:37 +0200

    Implemented a benchmark loop and more test functions.
    Benchmark now runs through (1) all the test functions, (2) maximum error
    tolerance values and both types of box_splitters.
    
    Test functions now include, apart from gaussian: distance from line,
    distance from points and Schwefel's function.

--- benchmark/benchmark.cpp
+++ benchmark/benchmark.cpp
@@ -3,6 +3,10 @@
 #include <string>
 #include <cmath>
 
+#include <CGAL/squared_distance_3.h>
+#include <CGAL/Point_3.h>
+#include <CGAL/Line_3.h>
+
 // HACK: copied imports from kd_tree.h to suppress 'redeclared with different access' exception
 #include <functional>
 #include <memory>
@@ -110,45 +114,118 @@ std::pair<double, double> tree_error(std::unique_ptr<kd_tree<double>> const &tre
     return std::make_pair(max_err, m);
 }
 
-int main(int /*argc*/,
-         char* /*argv*/[])
+typedef CGAL::Point_3<Kernel> Point_3;
+typedef CGAL::Line_3<Kernel> Line_3;
+
+Function3D<double> get_function(size_t id) {
+    switch (id) {
+        case 0:
+            // a gauss function around the (0,0,0) with 1-sigma equal to 0.1
+            return [](double x, double y, double z) {
+                return exp(-(x*x+y*y+z*z)/(2*0.1));
+            };
+        case 1:
+            // proportional to distance from a line
+            return [](double x, double y, double z) {
+                Point_3 point(x, y, z);
+                Point_3 p(-1., -1., -0.5);
+                Point_3 q( 0.5, 1.,  0.);
+                Line_3 line(p, q);
+
+                return sqrt(CGAL::squared_distance<Kernel>(point, line));
+            };
+        case 2:
+            // proportional to distance from three points
+            return [](double x, double y, double z) {
+                Point_3 point(x, y, z);
+                Point_3 p( 0.5,  0.5,  0.5);
+                Point_3 q( 0.5,  0.5, -0.5);
+                Point_3 r( 0.0,  0.0,  0.0);
+
+                return (sqrt(CGAL::squared_distance<Kernel>(point, p)) +
+                       sqrt(CGAL::squared_distance<Kernel>(point, q)) +
+                       sqrt(CGAL::squared_distance<Kernel>(point, r))) / 3;
+            };
+        case 3:
+            // Schwefel's function
+            return [](double x, double y, double z) {
+                double s = 0;
+
+                s += -x * sin(sqrt(std::abs(x*500)));
+                s += -y * sin(sqrt(std::abs(y*500)));
+                s += -z * sin(sqrt(std::abs(z*500)));
+
+                return s / 3;
+            };
+        default:
+            // constant zero function
+            return [](double, double, double) {
+                return 0;
+            };
+    }
+}
+
+int main(int argc,
+         char* argv[])
 {
+    if (argc != 3) {
+        printf("Usage: bin/benchmark <fun_from> <fun_to>\n");
+        exit(1);
+    }
+
+    size_t from = atoi(argv[1]);
+    size_t to = atoi(argv[2]);
+
+    assert(from <= to);
+    assert(to <= 3);
+
     feenableexcept(FE_INVALID | FE_DIVBYZERO | FE_OVERFLOW | FE_UNDERFLOW);
 
     std::unique_ptr<kd_tree<double>> tree;
 
     // volume represented by the tree
     Bbox_3 kd_tree_box = {-1, -1, -1, 1, 1, 1};
-    // approximated function
-    auto function = [](double x, double y, double z) {
-        return exp(-(x*x+y*y+z*z)/(2*0.1));
-    };
-    // maximum allowed error. Note: actual error depends on the accuracy
-    // of the ErrorEstimator template parameter of the kd_tree.
-    double required_accuracy = 0.2;
 
-    typedef decltype(function(0,0,0)) ElementT;
+    std::vector<double> tolerance = {1, .9, .8, .7, .6, 0.5, 0.4, 0.3, 0.2};
 
-    // tree construction
-    tree = kd_tree<ElementT,
-                   half_box_splitter<ElementT>
-                  >::build(kd_tree_box, function, required_accuracy);
+    for (size_t fun_i = from; fun_i <= to; fun_i++) {
+        Function3D<double> fun = get_function(fun_i);
+        std::for_each(tolerance.begin(), tolerance.end(), [&](const double tolerance) {
+            tree = kd_tree<double,
+                           half_box_splitter<double>
+                          >::build(kd_tree_box, fun, tolerance);
+            size_t node_count = count_nodes(tree);
+            std::pair<double, double> balance = balance_factor(tree);
+            std::pair<double, double> error = tree_error(tree, fun);
 
-    printf("%ld\n", count_nodes(tree));
-    std::pair<double, double> balance = balance_factor(tree);
-    printf("%f %f\n", balance.first, balance.second);
-    std::pair<double, double> error = tree_error(tree, function);
-    printf("%f %f\n", error.first, error.second);
+            printf("half %ld %f %ld %f %f %f %f\n",
+                fun_i,
+                tolerance,
+                node_count,
+                balance.first,
+                balance.second,
+                error.first,
+                error.second);
 
-    tree = kd_tree<ElementT,
-                   gradient_box_splitter<ElementT, 10>
-                  >::build(kd_tree_box, function, required_accuracy);
 
-    printf("%ld\n", count_nodes(tree));
-    balance = balance_factor(tree);
-    printf("%f %f\n", balance.first, balance.second);
-    error = tree_error(tree, function);
-    printf("%f %f\n", error.first, error.second);
+            tree = kd_tree<double,
+                           gradient_box_splitter<double, 10>
+                          >::build(kd_tree_box, fun, tolerance);
+
+            node_count = count_nodes(tree);
+            balance = balance_factor(tree);
+            error = tree_error(tree, fun);
+
+            printf("grad %ld %f %ld %f %f %f %f\n",
+                fun_i,
+                tolerance,
+                node_count,
+                balance.first,
+                balance.second,
+                error.first,
+                error.second);
+        });
+    }
 
     return 0;
 }
